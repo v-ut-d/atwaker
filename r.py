90be7dac128fb5fd2849ab484e8e34bbc477af0a
Koto Imai <polina.balanyeva@gmail.com>
2021-02-09 13:26:08 +0900

    c

--- r.py
+++ r.py
@@ -4,5 +4,5 @@ import os
 def connect():
     return redis.from_url(
         url=os.environ.get('REDIS_URL'), # 環境変数にあるURLを渡す
-        decode_responses=True, # 日本語の文字化け対策のため必須
+        # decode_responses=True, # 日本語の文字化け対策のため必須
     )
