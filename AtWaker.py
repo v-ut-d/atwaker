c4cf3fdc49d8e6eec9dff0357f227010327e7d0d
PolinaBalanyeva <polina.balanyeva@gmail.com>
2021-03-31 15:00:34 +0900

    debug

--- AtWaker.py
+++ AtWaker.py
@@ -296,7 +296,7 @@ def rate_calc(db,dt):
             #     rate=400*np.e**(rate/400-1)
             # dbr.at[dt,xx]=int(rate+0.5)
         if True:
-            vperf=db[xx].values*timelapse.values[::-1]
+            vperf=(db[xx].values*timelapse)[::-1]
             vperf=vperf[numpy.logical_not(numpy.isnan(vperf))]
             vperfext=np.array(sorted([vperf[i//100]-S*np.log(i%100+1) for i in range(len(vperf)*100)])[::-1])
             ratenom=0
