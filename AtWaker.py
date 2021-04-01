971451d78522ea3f857f6f37765c6ea9ffb9bb7f
PolinaBalanyeva <polina.balanyeva@gmail.com>
2021-04-01 13:11:56 +0900

    debug

--- AtWaker.py
+++ AtWaker.py
@@ -274,9 +274,9 @@ def rate_calc(db,dt):
     dbr=get_cached_df('AtWaker_rate_'+str(serverid))
     I=1200
     N=10000
-    L=(np.log(N)/np.log(100))**0.5
+    L=np.log(N)/np.log(100)
     R=(15*10**12+490153)**(10/N)/10**(140/N)
-    S=I/sum([np.log(1+i/((N-1)/99)**L)*R**(i+1) for i in range(N)])*sum([R**(i+1) for i in range(N)])
+    S=I/sum([np.log(101-(101-(i+1)**(1/L)))*R**(i+1) for i in range(N)])*sum([R**(i+1) for i in range(N)])
     print(R,S)
     if len(dbr)>0:
         vlast=dbr.iloc[-1]
@@ -302,7 +302,7 @@ def rate_calc(db,dt):
         if True:
             vperf=np.array((db[xx].values*timelapse)[::-1],dtype=float)
             vperf=vperf[np.logical_not(np.isnan(vperf))]
-            vperfext=np.array(sorted([vperf[i//N]-S*np.log(1+(i%N)/((N-1)/99)**L) for i in range(len(vperf)*N)])[::-1])
+            vperfext=np.array(sorted([vperf[i//N]-S*np.log(101-(101-(i%N+1)**(1/L))) for i in range(len(vperf)*N)])[::-1])
             print(vperfext)
             ratenom=0
             rateden=0
